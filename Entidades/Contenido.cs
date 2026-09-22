4075a038c47d1c9e7e1c3e72278d8e401e6d3880
Jav991 <jgomezcomino04@gmail.com>
2026-09-22 13:51:00 +0200

    Parte 1 😎

---
+++ Entidades/Contenido.cs
@@ -0,0 +1,22 @@
+namespace  Contenido{
+    public abstract class Contenido
+    {
+        public string titulo { get; set; }
+        public int duracion { get; set; }
+        public int reproducciones { get; set; }
+        protected Contenido(string titulo, int duracion, int reproducciones)
+        {
+            this.titulo = titulo;
+            this.duracion = duracion;
+            this.reproducciones = reproducciones;
+        }
+        public abstract void Reproducir();
+
+        public virtual void mostrarInformacion()
+        {
+           Console.WriteLine($"Título: {titulo}");
+           Console.WriteLine($"Duración: {duracion}");
+           Console.WriteLine($"Reproducciones: {reproducciones}");
+        }
+    }
+}
