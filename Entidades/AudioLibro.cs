b2fcee2c4173968ba4f46b3381e1a6d21d1d8d38
ArmandoMerino-webCode <amerino381@gmail.com>
2026-09-23 13:19:26 +0200

    Parte 6 parte 3 sin errores

--- Entidades/AudioLibro.cs
+++ Entidades/AudioLibro.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Plataforma_Streaming.Interfaces;
 
 namespace Plataforma_Streaming.Entidades
 {
-    internal class Audiolibro : Contenido
+    internal class Audiolibro : Contenido, IDescargable
     {
        
        public Audiolibro(string titulo, int duracion,
@@ -16,7 +17,8 @@ namespace Plataforma_Streaming.Entidades
         public string autor { get; set; }
         public string narrador { get; set; }
 
-        
+        private bool estadoDescarga = false;
+
         public void mostrarAudoLibro()
         {
             Console.WriteLine(
@@ -26,6 +28,28 @@ namespace Plataforma_Streaming.Entidades
                $"Reproduciones: {this.reproducciones} \n"
             );
         }
+
+        public void descargar()
+        {
+            if (estadoDescarga == false)
+            {
+                estadoDescarga = true;
+                Console.WriteLine($"El audiolibro: {this.titulo} se ha descargado con exito, estado: {estadoDescarga}");
+            }
+            return;
+        }
+        public void eliminarDescarga()
+        {
+            if (estadoDescarga)
+            {
+                estadoDescarga = false;
+                Console.WriteLine($"El estado de la descarga del audilibro: {this.titulo} ha sido eliminado y deja de estar descargado., estado: {estadoDescarga} ");
+            }
+            return;
+        }
+
+
+
         public override void Reproducir()
         {
             this.reproducciones++;
